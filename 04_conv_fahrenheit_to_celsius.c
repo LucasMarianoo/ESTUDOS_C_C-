e35c653f9261847fc6961dbf0b752a99739879bb
Lucas Mariano <117375062+LucasMarianoo@users.noreply.github.com>
2023-05-09 05:38:28 -0300

    Add files via upload

---
+++ 04_conv_fahrenheit_to_celsius.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/* Ler uma temperatura em graus Fahrenheit e apresentá-la convertida em graus Celsius */
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    // variáveis
+    float fahrenheit, celsius;
+
+    // entrada de dados
+    printf("Digite a temperatura Fahrenheit para converter: ");
+    scanf("%f", &fahrenheit);
+
+    // conversão
+    celsius = (fahrenheit-32)*5/9;
+
+    printf("A temperatura convertida é: %f", celsius);
+    
+
+    return 0;
+}
